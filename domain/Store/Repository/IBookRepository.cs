6139ed62a349f79e957afae9d31c507b1ba62c0d
Artem Gorlov <artem0gorlov@gmail.com>
2021-04-06 10:14:48 +0300

    Создать каркас веб приложения

---
+++ domain/Store/Repository/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Repository
+{
+    public interface IBookRepository
+    {
+        /// <summary>
+        /// Получает все книги по заголовку
+        /// </summary>
+        /// <param name="titlePart">Вводимый Title</param>
+        /// <returns></returns>
+        List<Book> GetAllByTitle(string titlePart);
+    }
+}
---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using Store.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly List<Book> _books = new List<Book>{
+            new Book(1,"Refactoring"),
+            new Book(2,"C# in Depht"),
+            new Book(3,"CLR via C#")
+        };
+        public List<Book> GetAllByTitle(string titlePart)
+        {
+            return _books.Where(b => b.Title.Contains(titlePart)).ToList();
+        }
+    }
+}
