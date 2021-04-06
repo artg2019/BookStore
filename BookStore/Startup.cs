ac4ac00887d20a6ee7366fbc0846041988217235
Artem Gorlov <artem0gorlov@gmail.com>
2021-04-06 10:41:49 +0300

    Создать страницу поиска

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Store.Memory;
+using Store.Repository;
 
 namespace BookStore
 {
@@ -24,6 +26,7 @@ namespace BookStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IBookRepository, BookRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
